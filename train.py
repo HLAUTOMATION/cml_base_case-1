3a0dc4fd1b960172c6aa4c6150c02262bfdd1480
HLAUTOMATION <155465752+HLAUTOMATION@users.noreply.github.com>
2024-09-10 21:02:19 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,33 +1,36 @@
+
 import json
 import os
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 import numpy as np
 
 # Read in data
-X_train = np.genfromtxt("data/train_features.csv")
-y_train = np.genfromtxt("data/train_labels.csv")
-X_test = np.genfromtxt("data/test_features.csv")
-y_test = np.genfromtxt("data/test_labels.csv")
+X_train = np.genfromtxt("data/train_features.csv", delimiter=',')
+y_train = np.genfromtxt("data/train_labels.csv", delimiter=',')
+X_test = np.genfromtxt("data/test_features.csv", delimiter=',')
+y_test = np.genfromtxt("data/test_labels.csv", delimiter=',')
 
 # Fit a model
 depth = 5
 clf = RandomForestClassifier(max_depth=depth)
 clf.fit(X_train, y_train)
 
+# Get accuracy
 acc = clf.score(X_test, y_test)
-print(acc)
+print(f"Accuracy: {acc:.4f}")
 
-metrics = """
-Accuracy: {:10.4f}
+# Write accuracy to metrics file
+metrics = f"""
+Accuracy: {acc:.4f}
 
 ![Confusion Matrix](plot.png)
-""".format(acc)
+"""
 with open("metrics.txt", "w") as outfile:
     outfile.write(metrics)
 
-# Plot it
-disp = plot_confusion_matrix(clf, X_test, y_test, normalize="true", cmap=plt.cm.Blues)
+# Plot the confusion matrix
+disp = ConfusionMatrixDisplay.from_estimator(clf, X_test, y_test, normalize="true", cmap=plt.cm.Blues)
 plt.savefig("plot.png")
